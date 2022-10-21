97e3cc0af745ad07ab8eadec1237eba2c99daa91
Grytsak <hrytsak.oleh7@gmail.com>
2022-10-21 14:25:56 +0300

    feat(deploy): heroku

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 import express from "express"
 import connectDB from "./db/connect.js"
 import dotenv from 'dotenv'
@@ -18,6 +22,18 @@ app.use(cors())
 app.use('/api/users', usersRoutes)
 app.use('/api/notes', noteRoutes)
 app.use('/api/notebooks', notebookRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+
+    app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+    )
+  )
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 const start = async () => {
     try {
